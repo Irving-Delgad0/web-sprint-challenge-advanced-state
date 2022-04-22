b5c1acc5698dec1a1cb50ca8dfc7e600a6ed4f62
Irving Delgado <rvngdelgado@gmail.com>
2022-04-22 18:19:32 -0500

    completed all tasks, all test passing

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,34 +1,65 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {connect} from "react-redux"
+import {selectAnswer, postAnswer, fetchQuiz} from "../state/action-creators"
 
-export default function Quiz(props) {
+function Quiz(props) {
+  const {quiz, postAnswer, selectAnswer, selectedAnswer, fetchQuiz} = props
+
+  useEffect(() => {
+    fetchQuiz();
+  }, [])
+
+  const handleSelectClick = (id) => {
+    selectAnswer(id)
+  }
+
+  const handleOnSubmit = (evt) => {
+    evt.preventDefault()
+    postAnswer({
+      quiz_id: quiz.quiz_id,
+      answer_id: selectedAnswer
+    })
+  }
   return (
     <div id="wrapper">
       {
         // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
-        true ? (
+        quiz ? (
           <>
-            <h2>What is a closure?</h2>
+            <h2>{quiz.question}</h2>
 
             <div id="quizAnswers">
-              <div className="answer selected">
-                A function
-                <button>
-                  SELECTED
+              <div className={selectedAnswer === quiz.answers[0].answer_id ? "answer selected" : "answer"}>
+                {quiz.answers[0].text}
+                <button onClick={() => handleSelectClick(quiz.answers[0].answer_id)}>
+                  {selectedAnswer === quiz.answers[0].answer_id ? "SELECTED" : "Select"}
                 </button>
               </div>
 
-              <div className="answer">
-                An elephant
-                <button>
-                  Select
+              <div className={selectedAnswer === quiz.answers[1].answer_id ? "answer selected" : "answer"}>
+                {quiz.answers[1].text}
+                <button onClick={() => handleSelectClick(quiz.answers[1].answer_id)}>
+                  {selectedAnswer === quiz.answers[1].answer_id ? "SELECTED" : "Select"}
                 </button>
               </div>
             </div>
 
-            <button id="submitAnswerBtn">Submit answer</button>
+            <button onClick={handleOnSubmit} disabled={!selectedAnswer} id="submitAnswerBtn">Submit answer</button>
           </>
         ) : 'Loading next quiz...'
       }
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  return{
+    quiz: state.quiz,
+    quiz_id: state.quiz_id,
+    question: state.question,
+    answers: state.answers,
+    selectedAnswer: state.selectedAnswer
+  }
+}
+
+export default connect(mapStateToProps, {selectAnswer, postAnswer, fetchQuiz})(Quiz)
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,17 +1,43 @@
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
 
-export function moveCounterClockwise() { }
+import axios from "axios"
+import {
+  INPUT_CHANGE,
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  RESET_FORM,
+  SET_INFO_MESSAGE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER
+} from "./action-types"
 
-export function selectAnswer() { }
+export function moveClockwise() {
+  return {type: MOVE_CLOCKWISE}
+ }
 
-export function setMessage() { }
+export function moveCounterClockwise() { 
+  return {type: MOVE_COUNTERCLOCKWISE}
+}
 
-export function setQuiz() { }
+export function selectAnswer(id) { 
+  return {type: SET_SELECTED_ANSWER, payload: id}
+}
 
-export function inputChange() { }
+export function setMessage(message) { 
+  return {type: SET_INFO_MESSAGE, payload: message}
+}
 
-export function resetForm() { }
+export function setQuiz(quiz) { 
+  return {type: SET_QUIZ_INTO_STATE, payload: quiz}
+}
+
+export function inputChange(value) { 
+  return {type: INPUT_CHANGE, payload: value}
+}
+
+export function resetForm() { 
+  return {type: RESET_FORM}
+}
 
 // ❗ Async action creators
 export function fetchQuiz() {
@@ -19,21 +45,45 @@ export function fetchQuiz() {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
+    axios.get("http://localhost:9000/api/quiz/next")
+    .then(res => {
+      dispatch(setQuiz(res.data))
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 }
-export function postAnswer() {
+export function postAnswer({quiz_id, answer_id}) {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
+    axios.post("http://localhost:9000/api/quiz/answer", {quiz_id, answer_id})
+    .then(res => {
+      dispatch(fetchQuiz())
+      dispatch(setMessage(res.data.message))
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 }
-export function postQuiz() {
+export function postQuiz({question_text,true_answer_text, false_answer_text}) {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
+    axios.post(`http://localhost:9000/api/quiz/new`, {question_text,true_answer_text, false_answer_text,})
+    .then(res => {
+      dispatch(setMessage(`Congrats: "${res.data.question}" is a great question!`));
+      dispatch(resetForm())
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   }
-}
+
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
